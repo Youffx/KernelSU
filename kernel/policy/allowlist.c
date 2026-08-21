0f3f19028f9e83259f270c279cf8063a0cbea777
Youff <asa.jazal@gmail.com>
2026-08-21 11:47:04 +0000

    policy: fix task_work and put_task_struct for kernel 4.19
    Kernel 4.19 uses a bool parameter for task_work_add instead of
    TWA_RESUME flag. Also add sched/task.h include for put_task_struct.
    
    Signed-off-by: Youffx <asa.jazal@gmail.com>

--- kernel/policy/allowlist.c
+++ kernel/policy/allowlist.c
@@ -3,6 +3,7 @@
 #include <linux/rculist.h>
 #include <linux/mutex.h>
 #include <linux/task_work.h>
+#include <linux/sched/task.h>
 #include <linux/capability.h>
 #include <linux/compiler.h>
 #include <linux/fs.h>
@@ -473,7 +474,11 @@ void ksu_persistent_allow_list()
         goto put_task;
     }
     cb->func = do_persistent_allow_list;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
     if (task_work_add(tsk, cb, TWA_RESUME)) {
+#else
+    if (task_work_add(tsk, cb, true)) {
+#endif
         kfree(cb);
         pr_warn("save_allow_list add task_work failed\n");
     }
